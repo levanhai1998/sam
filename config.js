e2d789a9af3e2b7f4212276af5e05f6fba55644f
levanhai1998 <86667736+levanhai1998@users.noreply.github.com>
2022-03-08 19:25:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '💘em💘';
+const giftUrl = 'https://levanhai1998.github.io/chuc/';
+const eventName = '💘Chúc Mừng 8-3💘';
+const titleCard = '💘Tặng bà chủ nhỏ💘';
+const contentCard = 'Chúc em 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả trong tương lai 💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'nhung.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
